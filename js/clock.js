58fb2ae5346b65f99e1ca9fc2e122b91c028bdb4
Mateusz <149150055+lillialover@users.noreply.github.com>
2023-11-24 10:14:22 +0000

    date

--- js/clock.js
+++ js/clock.js
@@ -5,6 +5,9 @@
     const myNode = document.createElement("div");
     const digitalClock = document.getElementById("digitalClock")
     digitalClock.appendChild(myNode)
-    
+    const myDate = new Date();
+    console.dir(myDate);
+    myNode.innerHTML = myDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+
 
     })();
