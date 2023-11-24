6df40c4c11b67a264a46e427f1da9c3bfa7dd21f
Mateusz <149150055+lillialover@users.noreply.github.com>
2023-11-24 09:59:55 +0000

    folders

--- clock.js
+++
@@ -1,6 +0,0 @@
-(function(){ 
-
-
-    console.info("Hello JS"); 
-    
-    })();
---
+++ js/clock.js
@@ -0,0 +1,10 @@
+(function(){ 
+
+
+    console.info("Hello JS"); 
+    const myNode = document.createElement("div");
+    const digitalClock = document.getElementById("digitalClock")
+    digitalClock.appendChild(myNode)
+    
+
+    })();
